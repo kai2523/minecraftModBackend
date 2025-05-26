b8e0026a21a449e564f6b9c014b77e60204eecb2
Felix Weller <felix.weller04@gmail.com>
2025-05-26 15:28:39 +0200

    Scraping-Variante der Wiki-Integration

--- wiki_integration/get_wiki_information.py
+++ wiki_integration/get_wiki_information.py
@@ -1,195 +1,7 @@
-"""
-Copyright Felix Weller, 2025
-"""
-
-import requests
-import string
+import json
 import Levenshtein
+import requests
 from bs4 import BeautifulSoup
-
-import pickle
-
-
-def create_matching_dict(wiki_api_url):
-    """
-    Erstellt ein Dictionary, das die Seitentitel und Redirect-Titel für jede
-    Minecraft Wiki Seite als Basis für das Matching der Schlüsselwörter
-    aus den Nutzerfragen mit den Minecraft Wiki Seiten enthält
-
-    Args:
-     wiki_api_url (str): URL des Minecraft Wiki für den API call
-    Returns:
-     matching_dict (dicitonary): Alle Minecraft Wiki Seitetitel und Redirect-Titel
-    """
-
-    # Alle Seitentitel holen
-    page_titles = get_wiki_page_titles(wiki_api_url)
-    print(f"{len(page_titles)} Titel gefunden.")
-
-    # Matching Dictionary erstellen
-    matching_dict = {}
-    for page_title in page_titles:
-        # Alle Redirect-Titel holen
-        redirect_titles = get_wiki_page_redirect_titles(wiki_api_url, page_title)
-        section_infos = get_wiki_page_section_infos(wiki_api_url, page_title)
-
-        sections = []
-        for title, index in section_infos:
-            sections.append({
-                "title": title,
-                "index": index
-            })
-
-        matching_dict[page_title] = {
-            "redirects": redirect_titles,
-            "sections": section_infos
-        }
-
-    return matching_dict
-
-
-def get_wiki_page_titles(wiki_api_url):
-    """
-    Führt API Calls an das Minecraft Wiki aus, die den Titel jeder
-    Minecraft Wiki Seite zurückgeben
-
-    Args:
-     wiki_api_url (str): URL des Minecraft Wiki für den API call
-    Returns:
-     page_titles (list): Liste aller Minecraft Wiki Seitentitel
-    """
-
-    page_titles = []
-    letters = list(string.ascii_uppercase) + ["Ä", "Ö", "Ü"]
-    apcontinue = None
-
-    # Alle Seitentitel von der Minecraft Wiki API holen
-    for letter in letters:
-        while True:
-            headers = {
-                "User-Agent": "MinecraftVillagerBot"
-            }
-            params = {
-                "action": "query",
-                "format": "json",
-                "list": "allpages",
-                "apprefix": letter,
-                "apfilterredir": "nonredirects",
-                "aplimit": "max"
-            }
-
-            if apcontinue:
-                params["apcontinue"] = apcontinue
-
-            response = requests.get(wiki_api_url, headers=headers, params=params)
-
-            # Seitentitel sammeln
-            data = response.json()
-            pages = data.get("query", {}).get("allpages", [])
-            for page in pages:
-                page_title = page["title"].lower()
-
-
-                """
-                # Seiten, die mit "Version", "Vollversion" (etc.) beginnen, herausfiltern
-                if not page_title.startswith("Version"):
-                    page_titles.append(page_title)
-                """
-
-
-                page_titles.append(page_title)
-
-            # Prüfen, ob weitere Seitentitel in der Antwort vorhanden sind
-            if "continue" in data:
-                apcontinue = data["continue"]["apcontinue"]
-            else:
-                break
-
-    return page_titles
-
-
-def get_wiki_page_redirect_titles(wiki_api_url, page_title):
-    """
-    Führt einen API Call an das Minecraft Wiki aus, der alle Redirect-Titel für die
-    gegebene Minecraft Wiki Seite zurückgibt
-
-    Args:
-     wiki_api_url (str): URL des Minecraft Wiki für den API call
-     page_title (str): Title der gegebenen Minecraft Wiki Seite
-    Returns:
-     redirect_titles (list): Liste aller Redirect-Titel, die auf die gegebene
-                             Minecraft Wiki Seite führen
-    """
-
-    headers = {
-        "User-Agent": "MinecraftVillagerBot"
-    }
-    params = {
-        "action": "query",
-        "format": "json",
-        "list": "backlinks",
-        "bltitle": page_title,
-        "blfilterredir": "redirects",
-        "bllimit": "max"
-    }
-
-    response = requests.get(wiki_api_url, headers=headers, params=params)
-
-    # Redirect-Titel sammeln
-    data = response.json()
-    backlinks = data.get("query", {}).get("backlinks", [])
-    redirect_titles = []
-    for backlink in backlinks:
-        redirect_title = backlink["title"].lower()
-        redirect_titles.append(redirect_title)
-
-    return redirect_titles
-
-
-def get_wiki_page_section_infos(wiki_api_url, page_title):
-    """
-    Führt einen API Call an das Minecraft Wiki aus, der den Titel und alle Unterkapitel
-    und deren Indizes von der gegebenen Minecraft Wiki Seite zurückgibt
-
-    Args:
-     wiki_api_url (str): URL des Minecraft Wiki für den API call
-     page_title (str): Title der gegebenen Minecraft Wiki Seite
-    Returns:
-     section_infos (list): Liste aller Unterkapitel und deren Indizes der gegebenen
-                           Minecraft Wiki Seite
-    """
-
-
-    """
-    es kann sein, dass wg der Wikistruktur kein Index für eine Section vorhanden ist,
-    s. Seite "Handwerk"
-    """
-
-
-    # Alle Informationen zu den Unterkapiteln von der Minecraft Wiki API holen
-    headers = {
-        "User-Agent": "MinecraftVillagerBot"
-    }
-    params = {
-        "action": "parse",
-        "format": "json",
-        "prop": "sections",
-        "page": page_title,
-        "redirects": 1
-    }
-
-    response = requests.get(wiki_api_url, headers=headers, params=params)
-
-    # Titel und Indizes der Unterkapitel holen
-    data = response.json()
-    sections = data.get("parse", {}).get("sections", [])
-    section_infos = []
-    for section in sections:
-        title = section["line"]
-        index = section["index"]
-        section_infos.append((title, index))
-
-    return section_infos
 
 
 def match_key_words(matching_dict, key_words, max_distance = 0):
@@ -235,10 +47,6 @@ def get_wiki_page(wiki_api_url, matched_pages):
      html_pages (list): HTML der gematchten Minecraft Wiki Seiten
     """
 
-    """
-    noch Error-Handling einbauen falls page_title wg Technik Wiki etc. nicht funktioniert
-    """
-
     # Seiten von der Minecraft Wiki API holen
     html_pages = []
     for page_title in matched_pages:
@@ -250,7 +58,6 @@ def get_wiki_page(wiki_api_url, matched_pages):
             "format": "json",
             "prop": "text",
             "page": page_title,
-            # "section": 6,
             "redirects": 1
         }
 
@@ -353,7 +160,6 @@ def format_html(html_pages):
 
         # Verbleibenden HTML-Code mit Text ersetzen
         page_text = html_page.get_text()
-
         formatted_pages.append(page_text)
 
     return formatted_pages
@@ -363,18 +169,18 @@ if __name__ == "__main__":
     # API URL des offiziellen Minecraft Wiki
     wiki_api_url = "https://de.minecraftwiki.net/api.php"
 
-    # matching_dict = create_matching_dict(url_fandom_wiki)
-    """with open("matching_dict.pkl", "wb") as f:
-        pickle.dump(matching_dict, f)"""
+    # Matching Dictionary laden
+    with open("matching_dict.json", "r", encoding="utf-8") as f:
+        matching_dict = json.load(f)
 
-    with open("matching_dict.pkl", "rb") as f:
-        matching_dict = pickle.load(f)
-
+    # Schlüsselwörter matchen
     key_words = ["craften", "diamantspitzhacke", "diamant"]
     matched_pages = match_key_words(matching_dict, key_words)
-    print(matched_pages)
 
+    # Gematchte Wiki Seiten holen
     response = get_wiki_page(wiki_api_url, matched_pages)
+
+    # Wiki Seiten formattieren
     wiki_pages = format_html(response)
 
     for i, page in enumerate(wiki_pages, 1):
